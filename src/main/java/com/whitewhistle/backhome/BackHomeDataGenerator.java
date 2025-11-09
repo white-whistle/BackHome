c9410c4106936a4d47f25c6c789eb18c7760a25b
BLooperZ <blooperz@users.noreply.github.com>
2025-11-09 19:09:01 +0200

    Project from template

---
+++ src/main/java/com/whitewhistle/backhome/BackHomeDataGenerator.java
@@ -0,0 +1,11 @@
+package com.whitewhistle.backhome;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BackHomeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}

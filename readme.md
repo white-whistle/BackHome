153b28d405a9c4b5ffd3b50e0963edc109cf092c
2mer <kenyweb7@gmail.com>
2025-11-10 23:39:07 +0200

    worldgen wip

---
+++ readme.md
@@ -0,0 +1,10 @@
+### Roadmap
+- [ ] turtle block
+- [ ] turtle pickaxe
+- [ ] turtle armor equip
+- [ ] turtle armor render
+- [ ] turtle armor lock/ curse of binding when in dimension
+- [ ] world gen fix mob spawns and generate filled blocks
+- [ ] turtle armor DataComponents for storing home index and last works/position
+#### nice to have
+- [ ] invite player in
--- src/main/java/com/whitewhistle/backhome/world/HomeChunkGenerator.java
+++ src/main/java/com/whitewhistle/backhome/world/HomeChunkGenerator.java
@@ -3,6 +3,7 @@ package com.whitewhistle.backhome.world;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.registry.RegistryEntryLookup;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryOps;
@@ -13,7 +14,6 @@ import net.minecraft.world.Heightmap;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.biome.source.BiomeAccess;
-import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.biome.source.FixedBiomeSource;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.StructureAccessor;
@@ -45,47 +45,99 @@ public class HomeChunkGenerator extends ChunkGenerator {
     }
 
     @Override
-    public void buildSurface(ChunkRegion region, StructureAccessor structures, NoiseConfig noiseConfig, Chunk chunk) {
-
-    }
-
-    @Override
-    public void populateEntities(ChunkRegion region) {
-
-    }
-
-    @Override
-    public int getWorldHeight() {
-        return 0;
-    }
-
-    @Override
     public CompletableFuture<Chunk> populateNoise(Blender blender, NoiseConfig noiseConfig, StructureAccessor structureAccessor, Chunk chunk) {
+        // Parameters for sine wave
+        double amplitude = 20;  // how tall
+        double frequency = 32;  // how wide the waves are
+        int baseHeight = 64;    // average ground height
+
+        BlockState solid = Blocks.STONE.getDefaultState();
+        BlockState top = Blocks.GRASS_BLOCK.getDefaultState();
+        BlockState barrier = Blocks.BEDROCK.getDefaultState();
+
+        // Loop through local chunk coordinates (0..15)
+        BlockPos.Mutable mutable = new BlockPos.Mutable();
+        for (int localX = 0; localX < 16; localX++) {
+            for (int localZ = 0; localZ < 16; localZ++) {
+                // Convert to world coords
+                int worldX = chunk.getPos().getStartX() + localX;
+                int worldZ = chunk.getPos().getStartZ() + localZ;
+
+                // Sine wave height
+                // double height = baseHeight + amplitude * Math.sin(worldX / frequency) * Math.cos(worldZ / frequency);
+                // int intHeight = (int) Math.floor(height);
+
+                var isWall = (worldX % frequency == 0) || (worldZ % frequency == 0);
+                int intHeight = isWall ? 60 : 10;
+
+                // Fill blocks up to height
+                for (int y = chunk.getBottomY(); y <= intHeight; y++) {
+                    mutable.set(localX, y, localZ);
+                    BlockState state =
+                              y == chunk.getBottomY() ? barrier
+                            : isWall ? barrier
+                            : y == intHeight ? top
+                            : solid;
+
+                    if (y == chunk.getBottomY()) {
+                        state = barrier;
+                    } else if (isWall) {
+                        state = barrier;
+                    } else if (y == intHeight) {
+                        state = top;
+                    } else {
+                        state = solid;
+                    }
+
+                    chunk.setBlockState(mutable, state);
+                }
+            }
+        }
+
         return CompletableFuture.completedFuture(chunk);
     }
 
     @Override
+    public int getWorldHeight() {
+        return 384;
+    }
+
+    @Override
     public int getSeaLevel() {
-        return 0;
+        return 63;
     }
 
     @Override
     public int getMinimumY() {
-        return 0;
+        return -64;
     }
 
     @Override
     public int getHeight(int x, int z, Heightmap.Type heightmap, HeightLimitView world, NoiseConfig noiseConfig) {
-        return 0;
+        double amplitude = 20;
+        double frequency = 32;
+        int baseHeight = 64;
+        return (int) (baseHeight + amplitude * Math.sin(x / frequency) * Math.cos(z / frequency));
     }
 
     @Override
     public VerticalBlockSample getColumnSample(int x, int z, HeightLimitView world, NoiseConfig noiseConfig) {
-        return new VerticalBlockSample(0, new BlockState[0]);
+        int height = getHeight(x, z, Heightmap.Type.WORLD_SURFACE_WG, world, noiseConfig);
+        BlockState[] states = new BlockState[Math.max(1, height - getMinimumY() + 1)];
+        for (int i = 0; i < states.length; i++) {
+            states[i] = i == states.length - 1 ? Blocks.GRASS_BLOCK.getDefaultState() : Blocks.STONE.getDefaultState();
+        }
+        return new VerticalBlockSample(getMinimumY(), states);
     }
 
     @Override
-    public void appendDebugHudText(List<String> text, NoiseConfig noiseConfig, BlockPos pos) {
+    public void buildSurface(ChunkRegion region, StructureAccessor structures, NoiseConfig noiseConfig, Chunk chunk) {}
 
+    @Override
+    public void populateEntities(ChunkRegion region) {}
+
+    @Override
+    public void appendDebugHudText(List<String> text, NoiseConfig noiseConfig, BlockPos pos) {
+        text.add("HomeChunkGenerator: sine wave terrain");
     }
 }
